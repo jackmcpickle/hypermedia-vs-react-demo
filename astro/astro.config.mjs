916d092664094ba575b1c211c73820e199806474
Jack McNicol <jackmcpickle@gmail.com>
2024-04-11 01:09:02 +0930

    move folders

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-import react from '@astrojs/react';
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react(), alpinejs()],
+    integrations: [tailwind(), alpinejs()],
     output: 'server',
     adapter: cloudflare(),
 });
